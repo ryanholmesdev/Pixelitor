44fd1940180946d4b8ae2d9014b3aa5e731662c8
Ryan Holmes <ryanholmes1996@gmail.com>
2020-04-30 10:11:13 +0100

    Added bounds to the canvas Added some styling scroll bars to canvas

--- src/components/CanvasDocument/CanvasDocument.js
+++ src/components/CanvasDocument/CanvasDocument.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './CanvasDocument.scss';
 import Moveable from 'react-moveable';
 import Canvas from '../Canvas/Canvas';
@@ -7,19 +7,24 @@ const CanvasDocument = (props) => {
   const [canvasWidth, setCanvasWidth] = useState(500);
   const [canvasHeight, setCanvasHeight] = useState(600);
   const [canvasWrapperEle] = useState(React.createRef());
+  const pageWrapperEle = useRef(null);
+  const [moveableBounds, setMoveableBounds] = useState(null);
+
   const [target, setTarget] = React.useState();
   const [frame] = React.useState({
     translate: [0, 0],
     width: 100,
     height: 100,
   });
+
+  const [isDraggingCanvas, setIsDraggingCanvas] = useState(false);
   const [isResizing, setIsReszing] = React.useState(false);
   const [isAllowedToDraw, setIsAllowedToDraw] = useState(false);
 
   const minCanvasWidth = 20;
   const minCanvasHeight = 20;
-  const maxCanvasWidth = 700;
-  const maxCanvasHeight = 700;
+  const maxCanvasWidth = 1200;
+  const maxCanvasHeight = 1200;
 
   useEffect(() => {
     setTarget(canvasWrapperEle.current);
@@ -31,12 +36,27 @@ const CanvasDocument = (props) => {
   };
 
   useEffect(setIsUserAllowedToDraw, [props.activeToolName]);
+  useEffect(() => {
+    console.log('on load', pageWrapperEle.current);
+    const bounds = getMoveableBound();
+    setMoveableBounds(bounds);
+  }, []);
+
+  const getMoveableBound = () => {
+    const wrapper = pageWrapperEle.current;
+    if (wrapper) {
+      return { top: wrapper.offsetTop, left: wrapper.offsetLeft };
+    } else {
+      // use defaults.
+      return { top: 20, left: 75 };
+    }
+  };
 
   return (
-    <div className="page-wrapper">
+    <div className="page-wrapper" ref={pageWrapperEle}>
       <div
         ref={canvasWrapperEle}
-        className={`canvas-wrapper ${isResizing === 'Select Tool' ? 'resizing-active' : ''}`}
+        className={`canvas-wrapper ${isDraggingCanvas === true || isResizing === true ? 'active' : ''}`}
         style={{ width: `${canvasWidth}px`, height: `${canvasHeight}px` }}
       >
         {props.layers.map((layer) => {
@@ -62,10 +82,13 @@ const CanvasDocument = (props) => {
         resizable={props.activeToolName === 'Select Tool' ? true : false}
         keepRatio={false}
         throttleResize={0}
+        throttleDrag={1}
         renderDirections={['nw', 'n', 'ne', 'w', 'e', 'sw', 's', 'se']}
         edge={false}
         zoom={1}
         origin={false}
+        snappable={true}
+        bounds={moveableBounds}
         draggable={props.activeToolName === 'Select Tool' ? true : false}
         onResizeStart={({ setOrigin, dragStart }) => {
           setOrigin(['%', '%']);
@@ -99,10 +122,14 @@ const CanvasDocument = (props) => {
         }}
         onDragStart={({ set }) => {
           set(frame.translate);
+          setIsDraggingCanvas(true);
         }}
         onDrag={({ target, beforeTranslate }) => {
           frame.translate = beforeTranslate;
           target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+        }}
+        onDragEnd={() => {
+          setIsDraggingCanvas(false);
         }}
       />
     </div>
