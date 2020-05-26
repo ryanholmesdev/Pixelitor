da75d1e4d45f229a16feaa47e18e65efef49d8b3
Ryan Holmes <ryanholmes1996@gmail.com>
2020-05-26 10:01:54 +0100

    Centered canvas

--- src/data/ActiveSettings.js
+++ src/data/ActiveSettings.js
@@ -1,8 +1,8 @@
 const settings = {
   color: '#476797',
   brushSize: 18,
-  canvasWidth: 500,
-  canvasHeight: 500,
+  canvasWidth: 600,
+  canvasHeight: 600,
   minCanvasWidth: 50,
   minCanvasHeight: 50,
   maxCanvasWidth: 1000,
